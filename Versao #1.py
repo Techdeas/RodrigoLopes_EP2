948ad0a7a189288850a61f7915fafb860af88eb1
Techdeas <rorigo.catto@gmail.com>
2015-03-29 17:59:16 -0300

    Versão #1
    Descrição do projeto.

---
+++ Versao #1.py
@@ -0,0 +1,11 @@
+# Rodrigo Lopes Catto --- Design de Software
+'''
+    Projeto #2   Jogo da Forca
+    
+O programa constitui em um jogo da forca utilizando 
+o turtle, uma ferramenta grafica. Assim o programa 
+ira localizar uma lista com palavras, decodifica-las, 
+e sortear uma palavra para ser decifrada pelo usuario.
+Por meio de inputs o programa verifica se a letra inserida 
+esta ou nao na palavra.
+                                                '''
